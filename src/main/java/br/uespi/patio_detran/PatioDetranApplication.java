f7e9696fa3b8f709faca1c0a914a657f91819e60
Joaomarcellodev <joaomarcellobraz@gmail.com>
2024-10-12 12:33:59 -0300

    criacao do projeto + configuracao do banco

---
+++ src/main/java/br/uespi/patio_detran/PatioDetranApplication.java
@@ -0,0 +1,13 @@
+package br.uespi.patio_detran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatioDetranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatioDetranApplication.class, args);
+	}
+
+}
