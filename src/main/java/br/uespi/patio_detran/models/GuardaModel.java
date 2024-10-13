e8a43cfe107f993f01a0fd5aabce818a78608523
Joaomarcellodev <joaomarcellobraz@gmail.com>
2024-10-13 14:51:04 -0300

    criacao da classe GuardaModel

---
+++ src/main/java/br/uespi/patio_detran/models/GuardaModel.java
@@ -0,0 +1,70 @@
+package br.uespi.patio_detran.models;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "TB_GUARDA")
+public class GuardaModel implements Serializable {
+
+
+
+    // Atributos 
+
+    public static long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+
+    @Column
+    private String nome;
+
+    @Column
+    private String sexo;
+
+
+
+    // Getters and Setters 
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public static void setSerialVersionUID(long serialVersionUID) {
+        GuardaModel.serialVersionUID = serialVersionUID;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+}
