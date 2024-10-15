1db58ef9ec444801a77adde444d1eed9d76ec6a5
Joaomarcellodev <joaomarcellobraz@gmail.com>
2024-10-14 21:09:08 -0300

    Adicionado relacionamento do GuardaModel

--- src/main/java/br/uespi/patio_detran/models/GuardaModel.java
+++ src/main/java/br/uespi/patio_detran/models/GuardaModel.java
@@ -1,13 +1,19 @@
 package br.uespi.patio_detran.models;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -34,8 +40,9 @@ public class GuardaModel implements Serializable {
 
     // Relacionamento
 
-    
-
+     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+     @OneToMany(mappedBy = "multas",fetch = FetchType.LAZY)
+     private Set<GuardaModel> guardas = new HashSet<>();
 
 
     // Getters and Setters 
@@ -70,6 +77,14 @@ public class GuardaModel implements Serializable {
 
     public void setSexo(String sexo) {
         this.sexo = sexo;
+    }
+
+    public Set<GuardaModel> getGuardas() {
+        return guardas;
+    }
+
+    public void setGuardas(Set<GuardaModel> guardas) {
+        this.guardas = guardas;
     }
 
 }
