5778b35d9a20180dff736a99a0def2a0c31f8c34
Joaomarcellodev <joaomarcellobraz@gmail.com>
2024-10-14 21:37:04 -0300

    Adicionado a classe MultaRepository

---
+++ src/main/java/br/uespi/patio_detran/repositories/MultaRepository.java
@@ -0,0 +1,10 @@
+package br.uespi.patio_detran.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import br.uespi.patio_detran.models.MultaModel;
+import java.util.UUID;
+
+@Repository
+public interface MultaRepository extends JpaRepository<MultaModel, UUID> {
+}
